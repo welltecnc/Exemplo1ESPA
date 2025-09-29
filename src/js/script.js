2c6af30c3a8dbcafa1ae0d81d5ddb2e71222fee7
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-29 11:30:21 -0300

    Declaração e variaveis

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,20 @@
-alert("Hello World")
+// alert("Hello World")
+
+// DECLARAÇÃO E VARIAVEIS
+
+var nome = "Fiap teste";
+console.log(typeof nome);
+
+let nome1 = "10";
+console.log(typeof nome1);
+
+const nome2 = {}
+console.log(typeof nome2)
+
+let nome3 =[];
+console.log(typeof nome3)
+
+let nome4=true;
+console.log(typeof nome4)
+
+
